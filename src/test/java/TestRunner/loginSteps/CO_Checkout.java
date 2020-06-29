ffb40f8ba1ea5028ffc717eadf53d50db1dfcfbd
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-06-29 17:34:54 +0530

    Update CO_Checkout.java

--- src/test/java/TestRunner/loginSteps/CO_Checkout.java
+++ src/test/java/TestRunner/loginSteps/CO_Checkout.java
@@ -111,14 +111,14 @@ public class CO_Checkout extends SetupClass {
 				
 
 				Thread.sleep(2000);
-			    WebElement new_email_signup = driver.findElement(By.xpath("//*[@id='email_address']"));
+			    WebElement new_email_signup = driver.findElement(By.xpath("/html/body/div[2]/main/div/div/div[2]/div/form/fieldset/div[4]/div/input"));
 				Thread.sleep(2000);
 			    new_email_signup.sendKeys(full_email);
 				Thread.sleep(2000);
 				
 				// enter name
 
-			    WebElement new_fname_signup = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#firstname")));
+			    WebElement new_fname_signup = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/main/div/div/div[2]/div/form/fieldset/div[5]/div[1]/div/input")));
 				Thread.sleep(2000);
 			    new_fname_signup.sendKeys("Selenium");
 				Thread.sleep(2000);
