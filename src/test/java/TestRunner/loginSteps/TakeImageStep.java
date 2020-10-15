2819ff5a52f759a0fdd19b11082b07c4c85573eb
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-10-15 11:49:18 +0530

    Update TakeImageStep.java

--- src/test/java/TestRunner/loginSteps/TakeImageStep.java
+++ src/test/java/TestRunner/loginSteps/TakeImageStep.java
@@ -2,7 +2,9 @@ package TestRunner.loginSteps;
 
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-
+import java.io.File;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.WebDriver;
 import TestRunner.SetupClass;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
