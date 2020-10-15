8cd6bff2126e001134229141a0b84ef4c613edf3
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-10-15 11:45:32 +0530

    Update TakeImageStep.java

--- src/test/java/TestRunner/loginSteps/TakeImageStep.java
+++ src/test/java/TestRunner/loginSteps/TakeImageStep.java
@@ -1,15 +1,7 @@
 package TestRunner.loginSteps;
 
-import java.io.File;
-import java.io.IOException;
-
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.testng.ITestContext;
-import org.testng.ITestListener;
-import org.testng.ITestResult;
 
 import TestRunner.SetupClass;
 import cucumber.api.Scenario;
@@ -17,27 +9,20 @@ import cucumber.api.java.After;
 
 
 	public class TakeImageStep extends SetupClass {
-	WebDriver driver=null;
-	String filePath = "C:\\SCREENSHOTS";
-    @Override
-    @Given("^onTestFailure")
-    public void onTestFailure(ITestResult result) {
-    	System.out.println("***** Error "+result.getName()+" test has failed *****");
-    	String methodName=result.getName().toString().trim();
-        ITestContext context = result.getTestContext();
-       WebDriver driver = (WebDriver)context.getAttribute("driver");
-    	takeScreenShot(methodName, driver);
-    }
-    @Then("^takeScreenShot")
-    public void takeScreenShot(String methodName, WebDriver driver) {
-    	 File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-         //The below method will save the screen shot in d drive with test method name 
-            try {
+		String filePath = "C:\\SCREENSHOTS";
+		@After
+		public void tearDown(Scenario scenario) {
+		    if (scenario.isFailed()) {	    	
+		      log.info("Scenario failed, now taking screenshot");
+		      File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			    try {
 				FileUtils.copyFile(scrFile, new File(filePath+methodName+".png"));
 				System.out.println("***Placed screen shot in "+filePath+" ***");
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-    }
-
+			    
+		      scenario.embed(screenshot, "image/png");
+		    }
+		}
 	}
