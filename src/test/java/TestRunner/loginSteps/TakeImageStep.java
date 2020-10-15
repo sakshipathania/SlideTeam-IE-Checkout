95b4b1067cee7b592e3339893bebd0da12817333
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-10-15 14:51:14 +0530

    Update TakeImageStep.java

--- src/test/java/TestRunner/loginSteps/TakeImageStep.java
+++ src/test/java/TestRunner/loginSteps/TakeImageStep.java
@@ -2,35 +2,19 @@ package TestRunner.loginSteps;
 
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import java.io.File;
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.By;
 
+import TestRunner.SetupClass;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
-import TestRunner.SetupClass;
 
 
 	public class TakeImageStep extends SetupClass {
-		String filePath = "C:/SCREENSHOTS";
-		
 		@After
-		
 		public void tearDown(Scenario scenario) {
-			
-		    if (scenario.isFailed()) {	
-			    try 
-                     {
+		    if (scenario.isFailed()) {	    	
 		      log.info("Scenario failed, now taking screenshot");
-		      File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-			FileUtils.copyFile(scrFile, new File(filePath+".png"));
-			System.out.println("***Placed screen shot in "+filePath+" ***");
-	
-			} catch (Exception e)
-                        { } 
-                   
- 
+		      final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+		      scenario.embed(screenshot, "image/png");
 		    }
 		}
 	}
