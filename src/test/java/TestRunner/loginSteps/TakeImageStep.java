50516768221c51bae724d63a37d94f4af857781e
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-10-15 11:31:29 +0530

    Update TakeImageStep.java

--- src/test/java/TestRunner/loginSteps/TakeImageStep.java
+++ src/test/java/TestRunner/loginSteps/TakeImageStep.java
@@ -1,21 +1,38 @@
 package TestRunner.loginSteps;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
 
-import TestRunner.SetupClass;
-import cucumber.api.Scenario;
-import cucumber.api.java.After;
+import com.pack.sample.TestBase;
 
-
-	public class TakeImageStep extends SetupClass {
-		@After
-		public void tearDown(Scenario scenario) {
-		    if (scenario.isFailed()) {	    	
-		      log.info("Scenario failed, now taking screenshot");
-		      final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-		      scenario.embed(screenshot, "image/png");
-		    }
-		}
-	}
+public class TestListener implements ITestListener {
+	WebDriver driver=null;
+	String filePath = "C:\\SCREENSHOTS";
+    @Override
+    public void onTestFailure(ITestResult result) {
+    	System.out.println("***** Error "+result.getName()+" test has failed *****");
+    	String methodName=result.getName().toString().trim();
+        ITestContext context = result.getTestContext();
+       WebDriver driver = (WebDriver)context.getAttribute("driver");
+    	takeScreenShot(methodName, driver);
+    }
+    
+    public void takeScreenShot(String methodName, WebDriver driver) {
+    	 File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+         //The below method will save the screen shot in d drive with test method name 
+            try {
+				FileUtils.copyFile(scrFile, new File(filePath+methodName+".png"));
+				System.out.println("***Placed screen shot in "+filePath+" ***");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+    }
 
