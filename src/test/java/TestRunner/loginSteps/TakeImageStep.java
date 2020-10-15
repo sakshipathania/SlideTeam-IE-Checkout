82dfdc803fa23aa8f6b63016b0d8b86dd009a9e5
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-10-15 12:56:02 +0530

    Update TakeImageStep.java

--- src/test/java/TestRunner/loginSteps/TakeImageStep.java
+++ src/test/java/TestRunner/loginSteps/TakeImageStep.java
@@ -13,16 +13,7 @@ import cucumber.api.java.After;
 
 	public class TakeImageStep extends SetupClass {
 		String filePath = "C:\\SCREENSHOTS";
-		WebDriver driver=null;
 		
-		
-		public void onTestFailure(String result) {
-         	System.out.println("***** Error "+result.getName()+" test has failed *****");
-    	        String methodName = result.getName().toString().trim();
-                // ITestContext context = result.getTestContext();
-                // WebDriver driver = (WebDriver)context.getAttribute("driver");
-    	         TakesScreenShot(methodName, driver);
-    }
 		@After
 		
 		public void tearDown(Scenario scenario) {
@@ -30,9 +21,8 @@ import cucumber.api.java.After;
 		    if (scenario.isFailed()) {	    	
 		      log.info("Scenario failed, now taking screenshot");
 		      File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-			    try {
-				FileUtils.copyFile(scrFile, new File(filePath+methodName+".png"));
-				System.out.println("***Placed screen shot in "+filePath+" ***");
+			FileUtils.copyFile(scrFile, new File(filePath+".png"));
+			System.out.println("***Placed screen shot in "+filePath+" ***");
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
