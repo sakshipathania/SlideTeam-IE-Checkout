35d92e66070177044a5644c52726ed32e967170e
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-11-23 13:53:31 +0530

    Update Paypal_Checkout.java
    locators has been changed for Business Team and Education

--- src/test/java/TestRunner/loginSteps/Paypal_Checkout.java
+++ src/test/java/TestRunner/loginSteps/Paypal_Checkout.java
@@ -359,7 +359,7 @@ public class Paypal_Checkout extends SetupClass {
 		driver.get("https://www.slideteam.net/pricing");
 		       Thread.sleep(4000);
 		
-		WebElement Business_Team = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/main/div[2]/div/div[2]/div[2]/div[1]/div/div/button[2]")));
+		WebElement Business_Team = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[normalize-space()='Business Teams']")));
 				Thread.sleep(2000);
 		js.executeScript("arguments[0].scrollIntoView();",Business_Team);
 		Thread.sleep(3000);
@@ -387,7 +387,7 @@ public class Paypal_Checkout extends SetupClass {
 		Thread.sleep(7000);
 		driver.get("https://www.slideteam.net/pricing");
 		       Thread.sleep(4000);
-		WebElement Business_Team = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/main/div[2]/div/div[2]/div[2]/div[1]/div/div/button[2]")));
+		WebElement Business_Team = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[normalize-space()='Business Teams']")));
 				Thread.sleep(2000);
 		js.executeScript("arguments[0].scrollIntoView();",Business_Team);
 		Thread.sleep(3000);
@@ -415,7 +415,7 @@ public class Paypal_Checkout extends SetupClass {
 		Thread.sleep(7000);
 		driver.get("https://www.slideteam.net/pricing");
 		       Thread.sleep(4000);
-		WebElement Business_Team = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/main/div[2]/div/div[2]/div[2]/div[1]/div/div/button[2]")));
+		WebElement Business_Team = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[normalize-space()='Business Teams']")));
 				Thread.sleep(2000);
 		js.executeScript("arguments[0].scrollIntoView();",Business_Team);
 		Thread.sleep(3000);
@@ -443,7 +443,7 @@ public class Paypal_Checkout extends SetupClass {
 		Thread.sleep(7000);
 		driver.get("https://www.slideteam.net/pricing");
 		       Thread.sleep(4000);
-		WebElement Business_Team = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/main/div[2]/div/div[2]/div[2]/div[1]/div/div/button[2]")));
+		WebElement Business_Team = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[normalize-space()='Business Teams']")));
 				Thread.sleep(2000);
 		js.executeScript("arguments[0].scrollIntoView();",Business_Team);
 		Thread.sleep(3000);
@@ -471,7 +471,7 @@ public class Paypal_Checkout extends SetupClass {
 		Thread.sleep(7000);
 		driver.get("https://www.slideteam.net/pricing");
 		       Thread.sleep(4000);
-		WebElement Business_Team = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/main/div[2]/div/div[2]/div[2]/div[1]/div/div/button[2]")));
+		WebElement Business_Team = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[normalize-space()='Business Teams']")));
 				Thread.sleep(2000);
 		js.executeScript("arguments[0].scrollIntoView();",Business_Team);
 		Thread.sleep(3000);
@@ -499,7 +499,7 @@ public class Paypal_Checkout extends SetupClass {
 		Thread.sleep(7000);
 		driver.get("https://www.slideteam.net/pricing");
 		       Thread.sleep(4000);
-		WebElement Business_Team = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/main/div[2]/div/div[2]/div[2]/div[1]/div/div/button[2]")));
+		WebElement Business_Team = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[normalize-space()='Business Teams']")));
 				Thread.sleep(2000);
 		js.executeScript("arguments[0].scrollIntoView();",Business_Team);
 		Thread.sleep(3000);
@@ -527,7 +527,7 @@ public class Paypal_Checkout extends SetupClass {
 		Thread.sleep(7000);
 		driver.get("https://www.slideteam.net/pricing");
 		       Thread.sleep(4000);
-		WebElement Education = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/main/div[2]/div/div[2]/div[2]/div[1]/div/div/button[3]")));
+		WebElement Education = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[normalize-space()='Education']")));
 				Thread.sleep(2000);
 		js.executeScript("arguments[0].scrollIntoView();",Education);
 		Thread.sleep(3000);
@@ -555,7 +555,7 @@ public class Paypal_Checkout extends SetupClass {
 		Thread.sleep(7000);
 		driver.get("https://www.slideteam.net/pricing");
 		       Thread.sleep(4000);
-		WebElement Education = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/main/div[2]/div/div[2]/div[2]/div[1]/div/div/button[3]")));
+		WebElement Education = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[normalize-space()='Education']")));
 				Thread.sleep(2000);
 		js.executeScript("arguments[0].scrollIntoView();",Education);
 		Thread.sleep(3000);
@@ -583,7 +583,7 @@ public class Paypal_Checkout extends SetupClass {
 		Thread.sleep(7000);
 		driver.get("https://www.slideteam.net/pricing");
 		       Thread.sleep(4000);
-		WebElement Education = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/main/div[2]/div/div[2]/div[2]/div[1]/div/div/button[3]")));
+		WebElement Education = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[normalize-space()='Education']")));
 				Thread.sleep(2000);
 		js.executeScript("arguments[0].scrollIntoView();",Education);
 		Thread.sleep(3000);
@@ -611,7 +611,7 @@ public class Paypal_Checkout extends SetupClass {
 		Thread.sleep(7000);
 		driver.get("https://www.slideteam.net/pricing");
 		       Thread.sleep(4000);
-		WebElement Education = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/main/div[2]/div/div[2]/div[2]/div[1]/div/div/button[3]")));
+		WebElement Education = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[normalize-space()='Education']")));
 				Thread.sleep(2000);
 		js.executeScript("arguments[0].scrollIntoView();",Education);
 		Thread.sleep(3000);
